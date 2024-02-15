a0f912b2942d32a7dfef4855e50e9e69dccd2a53
Jacobo91 <jacobovelasquez1991@gmail.com>
2024-02-15 15:20:39 -0500

    fix scroll after stickyATC btn click

--- assets/atc-sticky.js
+++ assets/atc-sticky.js
@@ -1,41 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+
     var button = document.getElementById('myButton');
+    console.log('Este es el boton que existe:', button)
 
     if (button) {
         button.addEventListener('click', function () {
+            const header = document.querySelector('.header');
+            const headerHeight = header.clientHeight;
+
             var section = document.getElementById('atc-section');
 
             if (section) {
-                section.scrollIntoView({
+                const sectionTop = section.getBoundingClientRect().top + window.scrollY;
+                const scrollToPosition = sectionTop - headerHeight;
+    
+                window.scrollTo({
+                    top: scrollToPosition,
                     behavior: 'smooth',
                 });
-            }
+            } 
+
         });
     }
 });
-
-/*
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.sticky-atc').style.opacity = '0';
-});
-
-
-window.addEventListener('scroll', function() {
-  var scroll = window.scrollY;
-  var headerHeight = document.querySelector('#atc-section').offsetHeight;
-  var stickyAtc = document.querySelector('.sticky-atc');
-
-  if (scroll > headerHeight) {
-      stickyAtc.style.opacity = '1';
-      stickyAtc.style.display = 'flex';
-  } else {
-      stickyAtc.style.opacity = '0';
-      stickyAtc.style.display = 'none';
-  }
-});
-*/
-
 
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.sticky-atc').style.opacity = '0';
@@ -55,3 +43,6 @@ window.addEventListener('scroll', function () {
         stickyAtc.style.display = 'none';
     }
 });
+
+
+
