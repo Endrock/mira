34bce47583430804dc2864cbcb55367e9ac87567
poma-endrock-software <david.pomareda@endrock.software>
2024-02-21 16:58:40 -0500

    add new button to sticky

--- assets/atc-sticky.js
+++ assets/atc-sticky.js
@@ -24,54 +24,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.sticky-atc').style.opacity = '0';
-
-    function scrollDetect(){
-        var lastScroll = 0;
-      
-        window.onscroll = function() {
-            let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-            var topVariation = document.querySelector("[data-sticky-top] .sticky-atc");
-      
-            if (currentScroll > 0 && lastScroll <= currentScroll){
-              lastScroll = currentScroll;
-              topVariation.style.top = '0px'; 
-            }else{
-              lastScroll = currentScroll;
-              topVariation.style.top = '70px'; 
-            }
-        };
-      }    
-      
-    scrollDetect();
-
-    let options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: 1.0,
-    };
-    
-    let target = document.querySelector(".product-form__buttons");
-    let observer = new IntersectionObserver( function(entries){
-        var moveBtn = document.querySelector("[data-sticky-bottom] .needsclick");
-        var stickyAtc = document.querySelector('.sticky-atc');
-
-        entries.forEach((entry) => {
-            if (entry.isIntersecting){
-                stickyAtc.style.opacity = '0';
-                stickyAtc.style.display = 'none';
-                moveBtn.style.bottom = '0px';
-            }
-            else {
-                stickyAtc.style.opacity = '1';
-                stickyAtc.style.display = 'flex';    
-                moveBtn.style.bottom = '90px';
-            }
-        });
-    } , options );
-
-    observer.observe(target);
 });
 
+document.addEventListener("scroll", function() {
+    var moveBtn = document.querySelector(".needsclick");
+    var scroll = window.scrollY;
+    var atcSectionPosition = 1300 ;
+    var stickyAtc = document.querySelector('.sticky-atc');
+    
+    if (scroll > atcSectionPosition) {
+        moveBtn.style.bottom = "90px"
+        stickyAtc.style.opacity = '1';
+        stickyAtc.style.display = 'flex';
+    } else {
+        stickyAtc.style.opacity = '0';
+        stickyAtc.style.display = 'none';
+        moveBtn.style.bottom = "0px"
+    }
+});
 
 
 
